762c5dc0b86d784d42baae29feed4b10a6ff0419
sukrubeyy <sukru.beyy@outlook.com>
2023-06-03 16:41:28 +0300

    update only one collectionElement system

--- Assets/Scripts/Editor/MongoEditor/MongoListView.cs
+++ Assets/Scripts/Editor/MongoEditor/MongoListView.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Extentions;
 using Mongo;
 using MongoDB.Bson;
@@ -25,6 +26,7 @@ public class MongoListView : EditorWindow
     private List<string> collectionData;
     Vector2 scrollPosition;
     private List<string> elements;
+    private Dictionary<string, List<string>> PreviousTextInput;
 
     [MenuItem("Tool/MongoList Editor")]
     public static void Initialize()
@@ -34,6 +36,7 @@ public class MongoListView : EditorWindow
         Window.PrepareData();
         Window.Show();
     }
+
     private void OnFocus()
     {
         if (Window != null || _init)
@@ -42,27 +45,58 @@ public class MongoListView : EditorWindow
         Window = GetWindow<MongoListView>();
         Window.PrepareData();
     }
+
     public void PrepareData()
     {
         //Tüm Json içindeki dataları burdan çek.
         string filePath = Path.Combine(Application.dataPath, "MongoData.json");
-
+        elements = new();
+        PreviousTextInput = new();
         if (File.Exists(filePath))
         {
             var jsonContent = File.ReadAllText(filePath);
             _mongoDatabases = BsonSerializer.Deserialize<MongoDatabases>(jsonContent);
             selectedDatabase = _mongoDatabases.databases[0];
             selectedCollection = selectedDatabase.collections[0];
+            PrepareElementList(selectedCollection);
         }
         else
             Debug.Log("Json File Does Not Exist" + filePath);
     }
+
+    private void PrepareElementList(MongoDatabases.Database.Collection collection)
+    {
+        PreviousTextInput.Clear();
+        elements.Clear();
+
+        foreach (var bsonValue in collection.elements)
+        {
+            foreach (var item in bsonValue.Values)
+            {
+                elements.Add(item.ToString());
+            }
+        }
+
+        foreach (var element in collection.elements)
+        {
+            List<string> elementString = new List<string>();
+            foreach (var itemValue in element.Values)
+            {
+                if (element["_id"].ToString() != itemValue)
+                    elementString.Add(itemValue.ToString());
+            }
+
+            PreviousTextInput.Add(element["_id"].ToString(), elementString);
+        }
+    }
+
     private void OnGUI()
     {
         if (_mongoDatabases == null)
             return;
 
         #region Window Style
+
         float padding = 10f;
         float lineWidth = 2f;
 
@@ -74,55 +108,72 @@ public class MongoListView : EditorWindow
         DrawVerticalLine(mainRect.yMax, lineWidth);
         DrawVerticalLine(rect1.yMax, lineWidth);
         DrawVerticalLine(rect2.yMax, lineWidth);
-        
+
         #endregion
+
         #region Save Operations
+
         GUILayout.BeginArea(mainRect);
         GUILayout.BeginHorizontal();
         if (GUILayout.Button("Save"))
         {
-
         }
+
         if (GUILayout.Button("Reset"))
         {
-
+            PrepareData();
         }
+
         GUILayout.EndHorizontal();
         GUILayout.EndArea();
+
         #endregion
+
         #region Choose Database
+
         GUILayout.BeginArea(rect1);
         foreach (var database in _mongoDatabases.databases)
             if (GUILayout.Button(database.name))
                 selectedDatabase = database;
         GUILayout.EndArea();
+
         #endregion
+
         #region Choose Collection
+
         GUILayout.BeginArea(rect2);
         foreach (var collection in selectedDatabase.collections)
             if (GUILayout.Button(collection.name))
+            {
                 selectedCollection = collection;
+                elements.Clear();
+                PrepareElementList(selectedCollection);
+            }
+
         GUILayout.EndArea();
+
         #endregion
 
         #region List Items
+
         GUILayout.BeginArea(rect3);
         scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Width(position.width), GUILayout.Height(150f));
 
         Color previousColor = GUI.color;
 
         int itemIndex = 1;
-
+        int collectionDataIndex = 0;
         foreach (var collection in selectedCollection.elements)
         {
             GUI.color = Color.yellow;
             GUILayout.Box("ITEM " + itemIndex);
             GUI.color = previousColor;
-            //int collectionDataIndex=0;
+            
             foreach (var collectionElement in collection.Values)
             {
-                string value = collectionElement.ToString();
-                value = EditorGUILayout.TextField(value);
+                if(collectionElement != collection["_id"])
+                    elements[collectionDataIndex] = EditorGUILayout.TextField(elements[collectionDataIndex]);
+                collectionDataIndex++;
             }
 
             EditorGUILayout.BeginHorizontal();
@@ -130,23 +181,34 @@ public class MongoListView : EditorWindow
             GUI.color = Color.green;
             if (GUILayout.Button("✓", GUILayout.Width(30), GUILayout.Height(30)))
             {
-                Debug.Log(collection["_id"]);
-                //TODO: UPDATE JSON
+                var objectID = collection["_id"].ToString();
+                var previousElementsValue = PreviousTextInput.FirstOrDefault(e => e.Key == objectID).Value;
+                foreach (var item in elements)
+                {
+                    if (item != objectID)
+                    {
+                        if (previousElementsValue.Contains(item))
+                        {
+                            //TODO: UPDATE JSON
+                            Debug.Log(item);
+                        }
+                    }
+                }
             }
-            
+
             GUI.color = Color.yellow;
             if (GUILayout.Button("<->", GUILayout.Width(30), GUILayout.Height(30)))
             {
-                //TODO: Reset Element Value JSON
+                PrepareData();
             }
-            GUI.color = Color.red;
 
+            GUI.color = Color.red;
             if (GUILayout.Button("X", GUILayout.Width(30), GUILayout.Height(30)))
             {
                 //Delete JSON
             }
-
             EditorGUILayout.EndHorizontal();
+            itemIndex++;
         }
 
         GUI.color = previousColor;
@@ -156,7 +218,6 @@ public class MongoListView : EditorWindow
         GUILayout.EndArea();
 
         #endregion
-
     }
 
     public Color GetRandomColor()
@@ -166,5 +227,6 @@ public class MongoListView : EditorWindow
         float b = UnityEngine.Random.Range(0f, 1f);
         return new Color(r, g, b);
     }
+
     private void DrawVerticalLine(float yPos, float width) => EditorGUI.DrawRect(new Rect(0, yPos, position.width, width), Color.black);
 }
--- Assets/Scripts/Mongo/MongoExtentions.cs
+++ Assets/Scripts/Mongo/MongoExtentions.cs
@@ -7,26 +7,24 @@ namespace Extentions
 {
     public static class MongoExtentions
     {
-        public static BsonDocument GetCollectionValues(this IMongoDatabase mongo, string collectionName)
+        #region For MongoDB ATLAS
+
+         public static BsonDocument GetCollectionValues(this IMongoDatabase mongo, string collectionName)
         { 
             BsonDocument doc = mongo.GetCollection<BsonDocument>(collectionName).Find(new BsonDocument()).FirstOrDefault();
             if (doc is null)
                 return null;
             return doc;
         }
-
         public static List<BsonDocument> GetCollectionAllValue(this IMongoDatabase database,string collectionName)
         {
             var collection = database.GetCollection<BsonDocument>(collectionName);
             return collection.Find(new BsonDocument()).ToList();
         }
-        
-
         public static void AddCollectionValue(this IMongoDatabase mongo, BsonDocument data, string collectionName)
         {
             mongo.GetCollection<BsonDocument>(collectionName).InsertOne(data);
         }
-
         public static void UpdateOne(this IMongoDatabase mongo, string columnName, string oldValue, string newValue)
         {
             //UPDATE
@@ -34,7 +32,6 @@ namespace Extentions
             var filter2 = Builders<BsonDocument>.Update.Set(columnName, newValue);
             mongo.GetCollection<BsonDocument>("userInfo").UpdateMany(filter, filter2);
         }
-
         public static void UpdateOne(this IMongoDatabase mongo, string columnName, int oldValue, int newValue)
         {
             //UPDATE
@@ -42,29 +39,24 @@ namespace Extentions
             var filter2 = Builders<BsonDocument>.Update.Set(columnName, newValue);
             mongo.GetCollection<BsonDocument>("userInfo").UpdateMany(filter, filter2);
         }
-
         public static void Delete(this IMongoDatabase mongo, string collectionName, string id, int searchValue)
         {
             //DELETE
             var filter3 = Builders<BsonDocument>.Filter.Eq(id, searchValue);
             mongo.GetCollection<BsonDocument>(collectionName).DeleteOne(filter3);
         }
-
         public static List<BsonDocument> GetAllCollections(this MongoClient client, BsonDocument doc)
         {
             return client.GetDatabase(doc["name"].AsString).ListCollections().ToList();
         }
-        
         public static List<string> GetAllCollectionNames(this MongoClient client, BsonDocument doc)
         {
             return client.GetDatabase(doc["name"].AsString).ListCollectionNames().ToList();
         }
-
         public static IAsyncCursor<BsonDocument> GetAllDatabases(this MongoClient client)
         {
             return client.ListDatabases();
         }
-        
         public static MongoClient ConnectionAccount(this MongoClient _client, string url)
         {
             if (!string.IsNullOrEmpty(url))
@@ -78,8 +70,6 @@ namespace Extentions
             }
             return null;
         }
-
-
         public static IMongoDatabase ConnectionDatabase(this MongoClient _client, string dbName)
         {
             if (_client != null && !string.IsNullOrEmpty(dbName))
@@ -89,5 +79,14 @@ namespace Extentions
 
             return null;
         }
+
+        #endregion
+
+
+        #region FOR JSON
+
+        
+
+        #endregion
     }
 }
