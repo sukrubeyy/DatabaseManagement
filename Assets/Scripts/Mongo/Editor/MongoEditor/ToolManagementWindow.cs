94e4796465abc4603fdf80c6568ba8440010023e
sukrubeyy <sukru.beyy@outlook.com>
2023-06-29 16:40:07 +0300

    - sqlite configuration basic

--- Assets/Scripts/Mongo/Editor/MongoEditor/ToolManagementWindow.cs
+++ Assets/Scripts/Mongo/Editor/MongoEditor/ToolManagementWindow.cs
@@ -1,4 +1,5 @@
 using System;
+using SQLite;
 using UnityEditor;
 using UnityEngine;
 
@@ -16,7 +17,7 @@ public class ToolManagementWindow : EditorWindow
     private SerializedProperty soConnectionUrl;
 
 
-    [MenuItem("Database Management/Database Connection",priority = 1)]
+    [MenuItem("Database Management/Database Connection", priority = 1)]
     public static void ShowWindow()
     {
         isInit = true;
@@ -76,6 +77,22 @@ public class ToolManagementWindow : EditorWindow
     private void SQLiteUI()
     {
         GUILayout.Space(10);
-        EditorGUILayout.HelpBox("Selamlar Ben SQLite",MessageType.Info);
+        EditorGUILayout.HelpBox("Selamlar Ben SQLite", MessageType.Info);
+        if (GUILayout.Button("Connection"))
+        {
+            SQLiteManagement sqLiteManagement = new SQLiteManagement();
+            sqLiteManagement.ConnectionDB();
+        }
+
+        if (GUILayout.Button("Add Item"))
+        {
+            SQLiteManagement sqLiteManagement = new SQLiteManagement();
+            sqLiteManagement.AddItem();
+        }
+
+        if (GUILayout.Button("Create Database"))
+        {
+            
+        }
     }
 }
--- Assets/Scripts/SQLite/SQLiteManagement.cs
+++ Assets/Scripts/SQLite/SQLiteManagement.cs
@@ -1,11 +1,15 @@
-using Mongo;
-using SQLite;
+using System.Data;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEngine;
+
 
 
 namespace SQLite
 {
     public class SQLiteManagement : DatabaseManager
     {
+        private SQLiteConnection connection;
+
         public override void Update(params object[] parameters)
         {
         }
@@ -21,5 +25,37 @@ namespace SQLite
         public override void Read(params object[] parameters)
         {
         }
+
+        public void ConnectionDB()
+        {
+            string databasePath = Application.dataPath + "/SQLite/userInfo.db";
+            string createTableQuery = "CREATE TABLE IF NOT EXISTS Person (Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Age INTEGER);";
+            connection = new SQLiteConnection(databasePath);
+            connection.Execute(createTableQuery);
+        }
+
+        public void AddItem()
+        {
+            Person person = new Person()
+            {
+                name = "memoli",
+                age = 31
+            };
+            string databasePath = Application.dataPath + "/SQLite/userInfo.db";
+            connection = new SQLiteConnection(databasePath);
+            connection.Insert(person);
+        }
+
+        public void CreateDatabase()
+        {
+            
+            
+        }
+    }
+
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
     }
 }
